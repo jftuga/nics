f01c2c3a7c4c576b4072c7ee13c242383dd34836
John Taylor <johntaylor@gmail.com>
2019-08-06 10:05:14 -0400

    added Gateway and DNS for Windows OS

--- nics.go
+++ nics.go
@@ -18,17 +18,145 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strconv"
 	"strings"
+	"syscall"
+	"unsafe"
 
 	"github.com/olekukonko/tablewriter"
 )
 
-const version = "1.1.0"
+const version = "1.2.0"
+
+const (
+	MAX_HOSTNAME_LEN    = 128
+	MAX_DOMAIN_NAME_LEN = 128
+	MAX_SCOPE_ID_LEN    = 256
+)
+
+// https://docs.microsoft.com/en-us/windows/win32/api/iptypes/ns-iptypes-ip_addr_string
+type ipAddressString struct {
+	next    *ipAddressString
+	address [16]byte
+	mask    [16]byte
+	context uint32
+}
+
+// https://docs.microsoft.com/en-us/windows/win32/api/iptypes/ns-iptypes-fixed_info_w2ksp1
+type fixedInfo struct {
+	hostName         [MAX_HOSTNAME_LEN + 4]byte
+	domainName       [MAX_DOMAIN_NAME_LEN + 4]byte
+	currentDNSServer *ipAddressString
+	dnsServerList    ipAddressString
+	nodeType         uint16
+	scopeID          [MAX_SCOPE_ID_LEN + 4]byte
+	enableRouting    uint16
+	enableProxy      uint16
+	enableDNS        uint16
+}
+
+const (
+	MAX_ADAPTER_NAME_LENGTH        = 256
+	MAX_ADAPTER_DESCRIPTION_LENGTH = 128
+	MAX_ADAPTER_ADDRESS_LENGTH     = 8
+)
+
+// https://docs.microsoft.com/en-us/windows/win32/api/iptypes/ns-iptypes-ip_adapter_info
+type ipAdapterInfo struct {
+	next               *ipAdapterInfo
+	comboIndex         uint32
+	adapterName        [MAX_ADAPTER_NAME_LENGTH + 4]byte
+	adapterDescription [MAX_ADAPTER_DESCRIPTION_LENGTH + 4]byte
+	addressLength      uint16
+	address            [MAX_ADAPTER_ADDRESS_LENGTH]byte
+	index              uint32
+	adapterType        uint32
+	dhcpEnabled        uint16
+	currentAddress     *ipAddressString
+	ipAddressList      ipAddressString
+	gatewayList        ipAddressString
+	dhcpServer         ipAddressString
+	haveWins           byte
+	primaryWins        ipAddressString
+	secondaryWins      ipAddressString
+	leaseObtained      uint64
+	leaseExpires       uint64
+}
+
+var (
+	getNetworkParams = syscall.NewLazyDLL("Iphlpapi.dll").NewProc("GetNetworkParams")
+	getAdaptersInfo  = syscall.NewLazyDLL("Iphlpapi.dll").NewProc("GetAdaptersInfo")
+)
+
+func sliceToString(slice []byte) string {
+	n := bytes.IndexByte(slice, 0)
+	return string(slice[:n])
+}
+
+func getDNSEntries() ([]string, error) {
+	err := getNetworkParams.Find()
+	if err != nil {
+		return nil, err
+	}
+
+	netInfo := [2]fixedInfo{}
+	size := unsafe.Sizeof(netInfo)
+
+	r0, _, err := getNetworkParams.Call(uintptr(unsafe.Pointer(&netInfo[0])), uintptr(unsafe.Pointer(&size)))
+	if r0 != 0 {
+		return nil, err
+	}
+
+	var dns1, dns2 string
+	dns1 = sliceToString(netInfo[0].dnsServerList.address[:])
+
+	nextDNS := netInfo[0].dnsServerList.next
+	if nextDNS != nil {
+		dns2 = sliceToString(nextDNS.address[:])
+	}
+
+	return []string{dns1, dns2}, nil
+}
+
+func getGateways() (map[string]string, error) {
+	err := getAdaptersInfo.Find()
+	if err != nil {
+		return nil, err
+	}
+
+	adapters := [16]ipAdapterInfo{}
+	size := unsafe.Sizeof(adapters)
+
+	result, _, err := getAdaptersInfo.Call(uintptr(unsafe.Pointer(&adapters[0])), uintptr(unsafe.Pointer(&size)))
+	if result != 0 {
+		return nil, err
+	}
+
+	ipMapGateway := make(map[string]string)
+
+	adapter := &adapters[0]
+	for adapter != nil {
+		/*
+			fmt.Println("  ip:", sliceToString(adapter.ipAddressList.address[:]))
+			fmt.Println("gate:", sliceToString(adapter.gatewayList.address[:]))
+			fmt.Println()
+		*/
+		ip := sliceToString(adapter.ipAddressList.address[:])
+		gate := sliceToString(adapter.gatewayList.address[:])
+		if ip != "0.0.0.0" {
+			ipMapGateway[ip] = gate
+		}
+
+		adapter = adapter.next
+	}
+
+	return ipMapGateway, nil
+}
 
 func isBriefEntry(ifaceName, macAddr, mtu, flags string, ipv4List, ipv6List []string, debug bool) bool {
 	if debug {
@@ -81,11 +209,11 @@ func extractIPAddrs(ifaceName string, allAddresses []net.Addr, brief bool) ([]st
 	return allIPv4, allIPv6
 }
 
-func networkInterfaces(brief bool, debug bool) {
+func networkInterfaces(brief bool, debug bool) ([]string, []string) {
 	adapters, err := net.Interfaces()
 	if err != nil {
 		fmt.Print(fmt.Errorf("%+v\n", err.Error()))
-		return
+		return nil, nil
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -96,11 +224,14 @@ func networkInterfaces(brief bool, debug bool) {
 		table.SetHeader([]string{"Name", "IPv4", "IPv6", "Mac Address", "MTU", "Flags"})
 	}
 
+	var v4Addresses []string
+	var v6Addresses []string
 	for _, iface := range adapters {
+		//fmt.Printf("%T %v\n", iface, iface)
 		allAddresses, err := iface.Addrs()
 		if err != nil {
 			fmt.Print(fmt.Errorf("%+v\n", err.Error()))
-			return
+			return nil, nil
 		}
 
 		allIPv4, allIPv6 := extractIPAddrs(iface.Name, allAddresses, brief)
@@ -119,15 +250,68 @@ func networkInterfaces(brief bool, debug bool) {
 		flags := iface.Flags.String()
 
 		if brief && isBriefEntry(ifaceName, macAddr, mtu, flags, allIPv4, allIPv6, debug) {
-			table.Append([]string{iface.Name, strings.Join(allIPv4, " "), macAddr, mtu, flags})
+			table.Append([]string{iface.Name, strings.Join(allIPv4, "\n"), macAddr, mtu, flags})
+			for _, ipWithMask := range allIPv4 {
+				ip := strings.Split(ipWithMask, "/")
+				v4Addresses = append(v4Addresses, ip[0])
+			}
 			continue
 		}
 
 		if !brief {
-			table.Append([]string{ifaceName, strings.Join(allIPv4, "\n"), strings.Join(allIPv6, "\n"), macAddr, mtu, strings.Replace(flags, "|", "\n", -1)})
 			table.SetAutoWrapText(true)
 			table.SetRowLine(true)
+			table.Append([]string{ifaceName, strings.Join(allIPv4, "\n"), strings.Join(allIPv6, "\n"), macAddr, mtu, strings.Replace(flags, "|", "\n", -1)})
+			for _, ipWithMask := range allIPv4 {
+				ip := strings.Split(ipWithMask, "/")
+				v4Addresses = append(v4Addresses, ip[0])
+			}
+			for _, ipWithMask := range allIPv6 {
+				ip := strings.Split(ipWithMask, "/")
+				v6Addresses = append(v6Addresses, ip[0])
+			}
 		}
+	}
+	table.Render()
+
+	return v4Addresses, v6Addresses
+}
+
+func arrayContains(value string, array []string) bool {
+	for _, a := range array {
+		if a == value {
+			return true
+		}
+	}
+	return false
+}
+
+func gatewayAndDNS(allIPv4, allIPv6 []string) {
+	var err error
+	var dns = []string{"N/A", "N/A"}
+	dns, err = getDNSEntries()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ipMapGateway := make(map[string]string)
+	ipMapGateway, err = getGateways()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAutoWrapText(false)
+	table.SetHeader([]string{"Gateway", "DNS1", "DNS2"})
+	for ip := range ipMapGateway {
+		if arrayContains(ip, allIPv4) {
+			table.Append([]string{ipMapGateway[ip], dns[0], dns[1]})
+		}
+		if arrayContains(ip, allIPv6) {
+			table.Append([]string{ipMapGateway[ip], dns[0], dns[1]})
+		}
+		dns[0] = ""
+		dns[1] = ""
 	}
 	table.Render()
 }
@@ -152,5 +336,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	networkInterfaces(!(*argsAllDetails), *argsDebug)
+	allIPv4, allIPv6 := networkInterfaces(!(*argsAllDetails), *argsDebug)
+	fmt.Println()
+	gatewayAndDNS(allIPv4, allIPv6)
 }
