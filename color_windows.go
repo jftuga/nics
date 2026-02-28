e5a2654fb4ec247ff53a603c179b05a7d490648a
John Taylor <jftuga@users.noreply.github.com>
2026-02-28 04:41:43 -0500

    fix: use kernel32.dll SetConsoleMode for Windows ANSI support (#15)

--- color_windows.go
+++ color_windows.go
@@ -5,14 +5,17 @@ package main
 import (
 	"os"
 	"syscall"
+	"unsafe"
 )
 
 const enableVirtualTerminalProcessing = 0x0004
+
+var setConsoleMode = syscall.NewLazyDLL("kernel32.dll").NewProc("SetConsoleMode")
 
 func init() {
 	handle := syscall.Handle(os.Stdout.Fd())
 	var mode uint32
 	if err := syscall.GetConsoleMode(handle, &mode); err == nil {
-		syscall.SetConsoleMode(handle, mode|enableVirtualTerminalProcessing)
+		setConsoleMode.Call(uintptr(handle), uintptr(unsafe.Pointer(uintptr(mode|enableVirtualTerminalProcessing))))
 	}
 }
